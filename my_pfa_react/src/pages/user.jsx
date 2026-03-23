e15a104f70a4d04ce38534dafed75e0737a6ecd6
hoyemm <hoyembenjemaa2004@gmail.com>
2026-03-23 13:30:03 +0100

    adding pages

---
+++ my_pfa_react/src/pages/user.jsx
@@ -0,0 +1,10 @@
+
+
+export function User() {
+    return (
+        <div className="user">
+            <h1>User Page</h1>
+            <p>This is the user page of our React application.</p>
+        </div>
+    );
+}
